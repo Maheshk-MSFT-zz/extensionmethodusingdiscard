22e21f0a971d961d61b3b4bdf72f88c6cdc82d24
Maheshkumar Rajarathinavel (AZURE) <maheshk@microsoft.com>
2019-06-08 15:42:04 +0530

    Add project files.

---
+++ extensionmethodVs2017/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//using Discards to ignore the out variable - https://blogs.msdn.microsoft.com/mazhou/2017/06/27/c-7-series-part-4-discards/
+
+namespace extensionmethodVs2017
+{
+    public static class ExtensionMethods
+    {
+        public static bool ToInteg(this string value)
+        {
+            return int.TryParse(value, out var _);
+        }
+    }
+}
---
+++ extensionmethodVs2017/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace extensionmethodVs2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someStringVariable = "100";
+
+            if (someStringVariable.ToInteg())
+            {
+                global::System.Console.WriteLine("Valid integer");
+            }
+            else
+            {
+                global::System.Console.WriteLine("Invalid integer");
+            }
+
+            _ = Console.ReadLine();
+        }
+    }
+}
